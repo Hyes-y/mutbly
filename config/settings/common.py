3e068a3d367e4e6cfca533f887dce3e9d3e8e073
Hye-S <gptjs7970@naver.com>
2022-02-13 03:42:13 +0900

    settings 변경

--- config/settings/common.py
+++ config/settings/common.py
@@ -23,15 +23,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
+'''
+# secrets.json 파일에서 SECRET_KEY 가져오기
 # SECRET_KEY 파일 위치
+
 secret_file = os.path.join(BASE_DIR, 'secrets.json')
 
 with open(secret_file) as f:
     secrets = json.loads(f.read())
-
-
-# secrets.json 파일에서 SECRET_KEY 가져오기
-'''
+    
 def get_secret(setting, secrets=secrets):
     try:
         return secrets[setting]
